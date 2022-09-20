2cf12de8a5744a6f639b974dd94cce9beca5a4ee
John Kenechukwu Okoye <93558021+johnkoye19@users.noreply.github.com>
2022-09-20 16:20:49 +0100

    Update 1-last_digit.py

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,19 +1,12 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last_digit = number % 10
-
-# YOUR CODE HERE
-This program will assign a random signed number to the variable number each time it is executed. Complete the source code in order to print the last digit of the number stored in the variable number.
-
-You can find the source code here
-The variable number will store a different value every time you will run this program
-You don’t have to understand what import, random.randint do. Please do not touch this code. This line should not change: number = random.randint(-10000, 10000)
-The output of the program should be:
-The string Last digit of, followed by
-the number, followed by
-the string is, followed by the last digit of number, followed by
-if the last digit is greater than 5: the string and is greater than 5
-if the last digit is 0: the string and is 0
-if the last digit is less than 6 and not 0: the string and is less than 6 and not 0
-followed by a new line
+last_digit = abs(number) % 10
+if (number < 0):
+        last_digit = last_digit * -1
+if (last_digit > 5):
+        print(f'Last digit of {number} is {last_digit} and is greater than 5')
+elif (last_digit == 0):
+        print(f'Last digit of {number} is {last_digit} and is 0')
+elif (last_digit < 6):
+        print(f'Last digit of {number} is {last_digit} and is less than 6 and not 0') 

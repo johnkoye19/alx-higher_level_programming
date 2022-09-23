d7f3e44bb50d080bf018e84b79e4347c610c63f4
Okoye John <okoyejohnkenechukwu@gmail.com>
2022-09-23 01:56:47 +0000

    p

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,11 +3,11 @@ if __name__ == "__main__":
     import sys
 ac = len(sys.argv)
 if ac == 2:
-    print("{:d} argument:".format(ac - 1))
+    print("1 argument:")
 elif ac == 1:
-    print("{:d} arguments.".format(ac -1))
+    print("0 arguments.")
 else:
-    print("{:d} arguments:".format(ac -1))
+    print("{} arguments:".format(ac -1))
 if ac > 1:
     for i in range(1, ac):
-        print("{:d}: {}".format(i, sys.argv[i]))
+        print("{}: {}".format(i, sys.argv[i]))

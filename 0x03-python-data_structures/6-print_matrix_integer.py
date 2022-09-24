3cfb033661b6874e4169c8b354174bb18651a918
Okoye John <okoyejohnkenechukwu@gmail.com>
2022-09-24 20:05:04 +0000

    kkf

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 # Write a function that prints a matrix of integers.
 
+
 def print_matrix_integer(matrix=[[]]):
     # Read through the matrix[][]
     for i in matrix:
         for a in i:
             # then print
-            print("{:d}".format(a), end = " ")
+            print("{:d}".format(a), end=" ")
         # newline
         print()

861c3cac5e62adc815f6dcd2909d087ec82ae9d9
John Kenechukwu Okoye <93558021+johnkoye19@users.noreply.github.com>
2022-09-24 10:12:43 +0100

    Update 5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 # Write a function that removes all characters c and C from a string.
+
 def no_c(my_string):
-	
-The function should return the new string
-You are not allowed to import any module
-You are not allowed to use str.replace()
+    new = ''
+    for i in my_string:
+        if (i == 'c') | (i == 'C'):
+            i = ''
+        new = new + i
+    return(new)

cec4643c31c1c97a3b1f90f81c5bcb2d5fdda4d3
John Kenechukwu Okoye <93558021+johnkoye19@users.noreply.github.com>
2022-09-26 03:38:02 +0100

    Update 7-add_tuple.py

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,29 +1,19 @@
 #!/usr/bin/python3
-# Write a function that adds 2 tuples.
-
 
 def add_tuple(tuple_a=(), tuple_b=()):
-    # add first elements
-    if (len(tuple_a) == 1):
-        c = 0
-        d = tuple_a[0]
-    elif (len(tuple_a) == 0):
-        c = 0
-        d = 0
-    else:
-        c = tuple_a[1]
-        d = tuple_a[0]
-    if (len(tuple_b) == 1):
-        e = 0
-        f = tuple_b[0]
-    elif (len(tuple_a) == 0):
-        e = 0
-        d = 0
-    else:
-        e = tuple_a[1]
-        f = tuple_a[0]
-    # the new tuple
-    a = d + f
-    b = c + e
-    tuple_c = (a, b)
-    return(tuple_c)
+    lenA = len(tuple_a)
+    lenB = len(tuple_b)
+# check for tuple_a
+    if lenA < 1:
+        tuple_a = (0, 0)
+    elif lenA < 2:
+        tuple_a = (tuple_a[0], 0)
+
+# check for tuple_b
+    if lenB < 1:
+        tuple_b = (0, 0)
+    elif lenB < 2:
+        tuple_b = (tuple_b[0], 0)
+
+    res = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+    return res

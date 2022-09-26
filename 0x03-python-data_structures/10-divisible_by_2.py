bd84aceb7d75dfc7ca032ca987701c22141d8a6f
John Kenechukwu Okoye <93558021+johnkoye19@users.noreply.github.com>
2022-09-26 03:42:17 +0100

    Update 10-divisible_by_2.py

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,11 +1,10 @@
-#!/usr/bin/pyhton3
-# Write a function that finds all multiples of 2 in a list.
-
+#!/usr/bin/python3
 
 def divisible_by_2(my_list=[]):
-    new_list = []
-    # read through my_list
-    for i in my_list:
-        # check if i is divisible by 2
-        new_list.append(i % 2 == 0)
-    return new_list
+    new_l = my_list.copy()
+    for i in range(0, len(my_list)):
+        if my_list[i] % 2 == 0:
+            new_l[i] = 1
+        else:
+            new_l[i] = 0
+    return new_l

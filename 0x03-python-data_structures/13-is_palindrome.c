ac8918cb45bb741d3c3dd8aa76b4277e95e2e62f
John Kenechukwu Okoye <93558021+johnkoye19@users.noreply.github.com>
2022-09-27 12:02:36 +0100

    Update 13-is_palindrome.c

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,1 +1,15 @@
-ghh
+Technical interview preparation:
+
+/**You are not allowed to google anything
+/* Whiteboard first
+/* Write a function in C that checks if a singly linked list is a palindrome.
+*/
+//how can I do this?
+//what if I read through the list?
+//how will I keep track of the numbers
+//or can I save them in a list as I read through?
+//or do we read it in tw parts. the first starts from the head node, while the second starts from the tail node?
+//then compare
+int is_palindrome(listint_t **head);
+Return: 0 if it is not a palindrome, 1 if it is a palindrome
+An empty list is considered a palindrome

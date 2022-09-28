c5d80e8af6ff19f697c30ace0231f5ae1a9e2edd
Okoye John <okoyejohnkenechukwu@gmail.com>
2022-09-28 21:44:33 +0000

    ll

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,1 +1,12 @@
-H
+#!/usr/bin/python3
+# Write a function that computes the square value of all integers of a matrix.
+
+
+def square_matrix_simple(matrix=[]):
+    # new list
+    new = []
+    # square each element
+    for i in matrix:
+        new.append([x**2 for x in i])
+    # return new list
+    return new

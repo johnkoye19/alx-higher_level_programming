8c7b2d2948d221ac43dea5807ccde9f0852f6dcf
John Kenechukwu Okoye <93558021+johnkoye19@users.noreply.github.com>
2022-09-29 14:14:49 +0100

    Create 9-multiply_by_2.py

---
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# Write a function that returns a new
+# dictionary with all values multiplied by 2
+
+
+def multiply_by_2(a_dictionary):
+You can assume that all values are only integers
+Returns a new dictionary
+You are not allowed to import any module

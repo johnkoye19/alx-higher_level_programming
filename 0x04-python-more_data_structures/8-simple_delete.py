1a622c097ecd2872dc4a534856476df84503adee
John Kenechukwu Okoye <93558021+johnkoye19@users.noreply.github.com>
2022-09-29 14:08:18 +0100

    Update 8-simple_delete.py

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -4,5 +4,8 @@
 
 
 def simple_delete(a_dictionary, key=""):
-	del a_dictionary[key]
-	return (a_dictionary)
+	if key in a_dictionary.keys()
+		del a_dictionary[key]
+		return (a_dictionary)
+	else:
+		return (a_dictionary)

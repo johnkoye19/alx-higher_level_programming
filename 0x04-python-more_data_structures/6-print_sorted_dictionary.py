74d974e9295ec4b0c39732a0d6f0335b3d08409c
Okoye John <okoyejohnkenechukwu@gmail.com>
2022-09-29 05:00:21 +0000

    sjf

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# Write a function that prints a dictionary by ordered keys.
+
+
+def print_sorted_dictionary(a_dictionary):
+    for i in sorted(a_dictionary):
+        print("{}: {}".format(i, a_dictionary[i]))

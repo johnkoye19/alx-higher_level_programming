1e1364a9844e0e989ac062c10aec080b854b3161
John Kenechukwu Okoye <93558021+johnkoye19@users.noreply.github.com>
2022-09-29 14:00:43 +0100

    Update 8-simple_delete.py

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,6 +1,8 @@
-Write a function that deletes a key in a dictionary.
+#!/usr/bin/python3
+# Write a function that deletes a
+# key in a dictionary.
 
-Prototype: def simple_delete(a_dictionary, key=""):
-key argument will be always a string
-If a key doesn’t exist, the dictionary won’t change
-You are not allowed to import any module
+
+def simple_delete(a_dictionary, key=""):
+	del a_dictionary[key]
+	return (a_dictionary)

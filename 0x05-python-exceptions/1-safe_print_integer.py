9bb62cf9815539142a84ed97340bc1eff6a39a45
Okoye <okoyejohnkenechukwu@gmail.com>
2022-10-10 16:33:59 +0100

    task 1

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+#write a function that prints integers only
+
+
+def safe_print_integer(value):
+    try:
+        print("{:d}".format(value))
+        return (True)
+    except ValueError:
+        return (False)

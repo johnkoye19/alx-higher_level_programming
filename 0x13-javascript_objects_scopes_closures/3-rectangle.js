34f058b36be8729b7422e53d5f37bec634595c0d
John <okoyejohnkenechukwu@gmail.com>
2022-12-20 16:46:13 -0800

    4

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -6,14 +6,17 @@ class Rectangle {
       this.height = h;
     }
   }
-  //instance method print
-  print(){
-      let post = '';
-      for (let i = 0; i < this.width; i++){
-          post = post + 'X';
-      }
-      for (let a = 0; a < this.height; a++){
-          console.log(post);
-      }
+
+  // instance method print
+  print () {
+    let post = '';
+    for (let i = 0; i < this.width; i++) {
+      post = post + 'X';
+    }
+    for (let a = 0; a < this.height; a++) {
+      console.log(post);
+    }
   }
 }
+
+module.exports = Rectangle;

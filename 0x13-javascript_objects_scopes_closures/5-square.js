993a3c3ea29e5976512e7a3e998825dcfc5e6372
Okoye <okoyejohnkenechukwu@gmail.com>
2022-12-21 11:19:46 +0100

    6

---
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,40 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (typeof w === 'number' && w > 0 && typeof h === 'number' && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  // instance method print
+  print () {
+    let post = '';
+    for (let i = 0; i < this.width; i++) {
+      post = post + 'X';
+    }
+    for (let a = 0; a < this.height; a++) {
+      console.log(post);
+    }
+  }
+
+  rotate () {
+    const a = this.width;
+    this.width = this.height;
+    this.height = a;
+  }
+
+  double () {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
+  }
+}
+
+class Square extends Rectangle {
+	constructor (size) {
+		super(w);
+		this.size = this.width;
+	}
+}
+
+module.exports = Rectangle;

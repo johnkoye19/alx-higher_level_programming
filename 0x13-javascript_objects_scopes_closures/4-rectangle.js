580b3d29db88dbeec99d158335aa8994f8662ed3
John Kenechukwu Okoye <93558021+johnkoye19@users.noreply.github.com>
2022-12-21 01:57:13 +0100

    Create 4-rectangle.js

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,31 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (typeof w === 'number' && w > 0 && typeof h === 'number' && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  // instance method print
+  print () {
+    let post = '';
+    for (let i = 0; i < this.width; i++) {
+      post = post + 'X';
+    }
+    for (let a = 0; a < this.height; a++) {
+      console.log(post);
+    }
+  }
+  rotate(){
+      let a = this.width;
+      this.width = this.height;
+      this.height = a;
+  }
+  double(){
+      this.width = this.width * 2;
+      this.height = this.height * 2;
+  }
+}
+
+module.exports = Rectangle;

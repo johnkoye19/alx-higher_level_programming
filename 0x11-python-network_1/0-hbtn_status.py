8e3fbf77ec6353c32c3e1b80735098ce91c6c2b5
John <okoyejohnkenechukwu@gmail.com>
2023-01-17 05:59:17 -0800

    task0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# we are trying to fetch a website using urllib
+
+import urllib.request
+
+with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as task0:
+    # print (type(task0))
+    # actual type
+    print ("Body response:")
+    actual_type = task0.read()
+    print ("\t- type: ", type(actual_type))
+    # actual content
+    print ("\t- content: ", actual_type)
+    # print string representation
+    string_type = actual_type.decode()
+    print ("\t- utf8 content: ", string_type)

61c1141a499dfb7afd792695d89114aa03778d6c
John <okoyejohnkenechukwu@gmail.com>
2023-01-17 06:28:34 -0800

    task0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,14 +4,14 @@ import urllib.request
 
 
 if __name__ == "__main__":
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as task0:
-        # print (type(task0))
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as t0:
+        # print (type(t0))
         # actual type
-        print ("Body response:")
-        actual_type = task0.read()
-        print ("\t- type: ", type(actual_type))
+        print("Body response:")
+        actual_type = t0.read()
+        print("\t- type: ", type(actual_type))
         # actual content
-        print ("\t- content: ", actual_type)
+        print("\t- content: ", actual_type)
         # print string representation
         string_type = actual_type.decode()
-        print ("\t- utf8 content: ", string_type)
+        print("\t- utf8 content: ", string_type)

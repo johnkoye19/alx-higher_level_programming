106c3ed7d00ccc9bf882b3258741e5370bddd93c
John Kenechukwu Okoye <93558021+johnkoye19@users.noreply.github.com>
2023-01-18 21:00:48 +0100

    Update 1-hbtn_header.py

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,2 +1,12 @@
 #!/usr/bin/python3
 """takes in a URL:sends a request to the URL:display X-Request-Id variable in header response"""
+import sys
+import urllib.request
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    request = urllib.request.Request(url)
+    with urllib.request.urlopen(request) as response:
+        print(dict(response.headers).get("X-Request-Id"))
